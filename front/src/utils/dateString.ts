51715795f3b5a4730edac54e72adc29c8f309e73
manist67 <manist67@naver.com>
2026-02-14 20:32:39 +0900

    feat : 시분초 한글로 변경

--- front/src/utils/dateString.ts
+++ front/src/utils/dateString.ts
@@ -1,22 +1,22 @@
 export const formatDuration = (seconds: number) => {
-  if(seconds == 0) return "0s"
+  if(seconds == 0) return "0초"
   const text: string[] = [];
   if (seconds >= 3600) {
-    text.push(`${Math.floor(seconds / 3600)}h`);
+    text.push(`${Math.floor(seconds / 3600)}시`);
     seconds = seconds % 3600;
   }
 
   if(seconds >= 60) {
     const remainMin = Math.floor(seconds / 60)
     if(remainMin != 0) {
-      text.push(`${remainMin}m`)
+      text.push(`${remainMin}분`)
     }
 
     seconds = seconds % 60
   }
 
   if(seconds > 0) {
-    text.push(`${seconds}s`)
+    text.push(`${seconds}초`)
   }
   
   return text.join(" ")
