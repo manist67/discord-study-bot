a1523ca06501adb1e729293a635ecf71d871af09
manist67 <manist67@naver.com>
2026-02-14 20:34:42 +0900

    feat : pretendard 폰트 설정

--- front/src/main.tsx
+++ front/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'pretendard/dist/web/static/pretendard.css'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router'
